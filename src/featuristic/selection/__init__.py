75c4ca1f2ce073fbf528ba7b72b45af507931c56
Martin Eastwood <martin.eastwood@gmx.com>
2025-07-20 21:05:04 +0100

    refactor: remove old genetic feature synthesis and selection modules

--- src/featuristic/selection/__init__.py
+++
@@ -1,3 +0,0 @@
-""" This module contains the feature selection algorithms. """
-
-from .genetic_feature_selection import GeneticFeatureSelector
--- src/featuristic/synthesis/__init__.py
+++
@@ -1,8 +0,0 @@
-""" This module contains the classes and functions for feature synthesis. """
-
-from .genetic_feature_synthesis import GeneticFeatureSynthesis
-from .fitness import fitness_pearson
-from .mrmr import MaxRelevanceMinRedundancy
-from .population import BasePopulation, ParallelPopulation, SerialPopulation
-from .program import node_count, random_prog, render_prog, select_random_node
-from .symbolic_functions import CustomSymbolicFunction, list_symbolic_functions
